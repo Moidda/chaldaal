f7b1da324f4e487c91e9f903c5b800eba2d37b2b
Mehedi Hasan <57999057+Moidda@users.noreply.github.com>
2020-11-16 22:29:55 +0600

    Home page shows product table
    -Home page now shows product table
    -search bar still not implemented

--- home/views.py
+++ home/views.py
@@ -5,15 +5,22 @@ from django.shortcuts import redirect
 
 def home(request):
     if 'email' in request.session:
-        customer_info = {}
-        customer_info['customer_id'] = request.session['customer_id']
-        customer_info['customer_name'] = request.session['customer_name']
-        customer_info['street_no'] = request.session['street_no']
-        customer_info['house_no'] = request.session['house_no']
-        customer_info['apt_no'] = request.session['apt_no']
-        customer_info['email'] = request.session['email']
-        customer_info['customer_credit'] = request.session['customer_credit']
-        customer_info['password'] = request.session['password']
-        return render(request, 'home_page.html', customer_info)
+        cursor = connection.cursor()
+        sql = 'SELECT * FROM PRODUCT'
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        context = []
+        for row in result:
+            product_id = row[0]
+            product_name = row[1]
+            unit = row[2]
+            units_in_stock = row[3]
+            price_per_unit = row[4]
+            category = row[5]
+            sub_category = row[6]
+            rating_by_customer = row[7]
+            context.append({'product_id':product_id, 'product_name':product_name, 'unit':unit, 'units_in_stock':units_in_stock, 'price_per_unit':price_per_unit, 'category':category, 'sub_category':sub_category, 'rating_by_customer':rating_by_customer})
+
+        return render(request, 'home_page.html', {'product' : context})
     else:
         return redirect('http://127.0.0.1:8000/log_in')
