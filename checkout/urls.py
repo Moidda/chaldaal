8db50d1a8c8792b59373bbe95302502139d979c7
Mehedi Hasan <57999057+Moidda@users.noreply.github.com>
2020-12-10 19:32:33 +0600

    Checkout, redeem points using js
    Redeeming points now does not reload the checkout page. Javascript added

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.checkout, name='checkout'), # /checkout/
     path('confirm_checkout/', views.confirm_checkout, name='confirm_checkout'), # /checkout/confirm_checkout
-    path('using_points/', views.using_points, name='using_points'), # checkout/using_points
+    path('ajax/using_points/', views.using_points, name='using_points'), # checkout/using_points
 ]
