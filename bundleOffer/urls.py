6f391ba42a5f498f8059803e22e05f44eca0c4bf
Mehedi Hasan <57999057+Moidda@users.noreply.github.com>
2020-12-10 16:28:31 +0600

    Backend work for bundleOffer required

--- bundleOffer/urls.py
+++ bundleOffer/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('', views.bundleOffer, name='bundle-offer'),
+    path('bundle-list/', views.bundle_list, name='bundle-list'),
+    path('bundle-end/<int:bundle_id>', views.bundle_end, name='bundle-end'),
 ]
--- bundleOffer/views.py
+++ bundleOffer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from cart.views import cart
 
 
@@ -13,3 +14,28 @@ def bundleOffer(request):
         'cart_price': cart.total_cost
     }
     return render(request, 'bundle-offer.html', context)
+
+
+def bundle_list(request):
+    if 'customer_id' not in request.session or request.session['customer_id'] != 1:
+        return redirect(home_page)
+    context = {
+        'customer_id': request.session['customer_id'],
+        'cart_price': cart.total_cost,
+        'bundle': [
+            {'bundle_id': 1, 'bundle_name': 'Bundle 1', 'bundle_cost': 320},
+            {'bundle_id': 2, 'bundle_name': 'Bundle 2', 'bundle_cost': 430}
+        ]
+    }
+    return render(request, 'bundle-list.html', context)
+
+
+def bundle_end(request, bundle_id):
+    if 'customer_id' not in request.session or request.session['customer_id'] != 1:
+        return redirect(home_page)
+
+    print("--------------------------------------------------------------------------------------------------")
+    print("Delete bundle offer with id = " + str(bundle_id) + " from database")
+    print("--------------------------------------------------------------------------------------------------")
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
